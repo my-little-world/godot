a38874cb8e5e1c1aed466f79c73db926672e9806
Joao V <29417278+joaovsq@users.noreply.github.com>
2023-04-18 22:43:32 -0400

    adjusting the build script

--- build.bash
+++ build.bash
@@ -5,6 +5,9 @@
 
 source env/bin/activate
 
+# Compile using platform libraries (https://docs.godotengine.org/en/latest/contributing/development/compiling/compiling_for_linuxbsd.html#requirements)
+# scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes builtin_embree=no builtin_enet=no builtin_freetype=no builtin_graphite=no builtin_harfbuzz=no builtin_libogg=no builtin_libpng=no builtin_libtheora=no builtin_libvorbis=no builtin_libwebp=no builtin_mbedtls=no builtin_miniupnpc=no builtin_pcre2=no builtin_zlib=no builtin_zstd=no
+
 scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes
 
 ./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
