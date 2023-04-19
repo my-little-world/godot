1365b03ac9bdddf41db8beb51a9cd5553dfa08e2
Joao V <29417278+joaovsq@users.noreply.github.com>
2023-04-18 23:00:01 -0400

    adjusting the build.bash script

--- build.bash
+++ build.bash
@@ -13,3 +13,5 @@ scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes
 ./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
 
 python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local ~/.nuget/GodotNET
+
+dotnet nuget add source ~/.nuget/GodotNET --name GodotNET
