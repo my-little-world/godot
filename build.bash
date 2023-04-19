2e57ba1b784773c92691eff9a35bf8c0127e8bc2
Joao V <29417278+joaovsq@users.noreply.github.com>
2023-04-18 23:09:30 -0400

    comments in the build.bash script

--- build.bash
+++ build.bash
@@ -5,13 +5,14 @@
 
 source env/bin/activate
 
-# Compile using platform libraries (https://docs.godotengine.org/en/latest/contributing/development/compiling/compiling_for_linuxbsd.html#requirements)
-# scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes precision=double builtin_embree=no builtin_enet=no builtin_freetype=no builtin_graphite=no builtin_harfbuzz=no builtin_libogg=no builtin_libpng=no builtin_libtheora=no builtin_libvorbis=no builtin_libwebp=no builtin_mbedtls=no builtin_miniupnpc=no builtin_pcre2=no builtin_zlib=no builtin_zstd=no
+# Another option is to compile using platform libraries (https://docs.godotengine.org/en/latest/contributing/development/compiling/compiling_for_linuxbsd.html#requirements)
 
-scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes precision=double
+# include the flag precision=double for double precision
+scons platform=linuxbsd module_mono_enabled=yes target=editor production=yes
 
 ./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
 
-python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local ~/.nuget/GodotNET --precision=double
+# include the flag --precision=double for double precision
+python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local ~/.nuget/GodotNET
 
 dotnet nuget add source ~/.nuget/GodotNET --name GodotNET
